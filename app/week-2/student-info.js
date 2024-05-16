2ac6ed3c057330f2f242ac53794430ec3786a1c3
rickyasuncion <ricky.asuncion@edu.sait.ca>
2024-05-16 09:43:59 -0700

    Initial commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+const { default: Link } = require("next/link");
+
+function StudentInfo() {
+    return (
+      <main>
+        <h1>Ricky Asuncion</h1>
+        <Link href={""}></Link>
+      </main>
+    );
+  }
