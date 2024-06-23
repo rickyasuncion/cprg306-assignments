b76e9ed03920b86058af9132470ddab8545c46a1
rickyasuncion <145637074+dootDoo@users.noreply.github.com>
2024-06-23 15:16:52 -0600

    week-7

--- app/page.js
+++ app/page.js
@@ -3,11 +3,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div class="z-10 max-w-5xl w-full items-center justify-between text-sm">
-        <h1 class="text-4xl font-bold mb-5">
+      <div className="z-10 max-w-5xl w-full items-center justify-between text-sm">
+        <h1 className="text-4xl font-bold mb-5">
           CPRG 306: Web Development 2 - Assignments 
         </h1>
-        <div class="text-lg">
+        <div className="text-lg">
           <ul>
             <li className="hover:text-green-400 hover:underline">
               <Link href="/week-2">Week 2 Assignment</Link>
---
+++ app/week-7/item.js
@@ -0,0 +1,18 @@
+const Items = ({ ItemList, onSelect }) => {
+  return ItemList.map((item) => (
+    <li 
+      className="p-2 m-4 bg-slate-900 justify-center mx-auto" 
+      key={item.id} 
+      onClick={() => onSelect(item)}
+    >
+      <ul>
+        <li className="text-x font-bold mb-4">{item.name}</li>
+        <li>
+          Buy {item.quantity} in {item.category}
+        </li>
+      </ul>
+    </li>
+  ));
+};
+
+export default Items;
