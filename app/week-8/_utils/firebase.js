6f677cf782175a4ec3357a7c9505a204c8e75ea2
rickyasuncion <145637074+dootDoo@users.noreply.github.com>
2024-07-02 11:39:13 -0600

    fix firebase api key

--- app/week-8/_utils/firebase.js
+++ app/week-8/_utils/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
  
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCePAIFAdejAVH0HrxL2PExVmFnHq5E8B0",
+  authDomain: "cprg306-assignments-28fc0.firebaseapp.com",
+  projectId: "cprg306-assignments-28fc0",
+  storageBucket: "cprg306-assignments-28fc0.appspot.com",
+  messagingSenderId: "527488498204",
+  appId: "1:527488498204:web:7f09b7eccea09827403dd9"
 };
  
 // Initialize Firebase
